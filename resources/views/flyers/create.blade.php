928ad8fb68f81fc6b1775cb135305b6faf67ae97
joseayram <jose@softwarecriollo.com>
2015-07-30 11:28:29 -0430

    Improve the styles sheets to the flyers create form

--- resources/views/flyers/create.blade.php
+++ resources/views/flyers/create.blade.php
@@ -3,12 +3,14 @@
 @section('content')
     <h1>Selling Your Home?</h1>
     <hr>
-    {!! Form::open(['method' => 'POST', 'url' => '/flyers', 'class' => 'form-horizontal', 'enctype' => 'multipart/form-data']) !!}
-        @include('flyers.form')
+    <div class="row">
+        {!! Form::open(['method' => 'POST', 'url' => '/flyers', 'class' => 'form-horizontal col-md-6', 'enctype' => 'multipart/form-data']) !!}
+            @include('flyers.form')
 
-        <div class="btn-group pull-right">
-            {!! Form::reset("Reset", ['class' => 'btn btn-warning']) !!}
-            {!! Form::submit("Create Flyer", ['class' => 'btn btn-success']) !!}
-        </div>
-    {!! Form::close() !!}
+            <div class="btn-group pull-right">
+                {!! Form::reset("Reset", ['class' => 'btn btn-warning']) !!}
+                {!! Form::submit("Create Flyer", ['class' => 'btn btn-success']) !!}
+            </div>
+        {!! Form::close() !!}
+    </div>
 @stop
